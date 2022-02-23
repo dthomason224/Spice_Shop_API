14134fe782d8935ddf4f90d3d621d00507a9eb93
Dalton <dthomason24@gmail.com>
2022-02-23 16:29:00 -0500

    init

---
+++ src/main/java/com/example/spice_shop_api/SpiceShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spice_shop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiceShopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiceShopApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spice_shop_api/SpiceShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spice_shop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiceShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
