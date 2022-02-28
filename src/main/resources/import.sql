89bf79ee433e3570eceaef546572b05e406af950
Dalton <dthomason24@gmail.com>
2022-02-28 15:33:30 -0500

    added cross orgin to controllers

---
+++ src/main/resources/import.sql
@@ -0,0 +1,44 @@
+INSERT INTO product (description, image, name, price, stock) VALUES
+    ('Fine-grained salt', '/src/assets/images/salt.jpg', 'Salt', 2.00, 50);
+
+INSERT INTO product (description, image, name, price, stock) VALUES
+    ('Fine-grained pink Himalayan salt', '/src/assets/images/pink_salt.jpg', 'Pink Salt', 6.50, 50);
+
+INSERT INTO product (description, image, name, price, stock) VALUES
+    ('Dried whole black peppercorns', '/src/assets/images/black_peppercorn.jpg', 'Black Peppercorns', 8.00 50);
+
+INSERT INTO product (description, image, name, price, stock) VALUES
+    ('Vanilla bean', '/src/assets/images/vanilla.jpg', 'Vanilla', 10.00, 25);
+
+INSERT INTO product (description, image, name, price, stock) VALUES
+    ('Seeds to add to a dish', '/src/assets/images/sesame_seeds.jpg', 'Sesame Seeds', 3.00, 50);
+
+INSERT INTO product (description, image, name, price, stock) VALUES
+    ('Made from slowly dried peppers', '/src/assets/images/paprika.jpg', 'Paprika', 7.95, 50);
+
+INSERT INTO product (description, image, name, price, stock) VALUES
+    ('Can be used by themselves or turned into a powder', '/src/assets/images/cumin.jpg', 'Cumin Seeds', 6.99, 50);
+
+INSERT INTO product (description, image, name, price, stock) VALUES
+    ('Delicate star anise pods', '/src/assets/images/star_anise.jpg', 'Star Anise', 7.50, 25);
+
+INSERT INTO product (description, image, name, price, stock) VALUES
+    ('Dried fruit from coriander plant', '/src/assets/images/coriander_seeds.jpg', 'Coriander Seeds', 4.99, 30);
+
+INSERT INTO product (description, image, name, price, stock) VALUES
+    ('Inner bark of the Cinnamomum tree', '/src/assets/images/cinnamon.jpg', 'Cinnamon Sticks', 7.99, 30);
+
+INSERT INTO category (description, name) VALUES
+    ('Yes', 'Seeds');
+
+INSERT INTO category (description, name) VALUES
+    ('Yes', 'Chile');
+
+INSERT INTO category (description, name) VALUES
+    ('Yes', 'Salt');
+
+INSERT INTO category (description, name) VALUES
+    ('Yes', 'Baking');
+
+INSERT INTO category (description, name) VALUES
+    ('Yes', 'Essential');
